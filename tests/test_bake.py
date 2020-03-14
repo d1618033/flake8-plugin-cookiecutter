88333cd37249231fe797fa9c6f750b0642881a4c
d1618033 <d1618033@gmail.com>
2020-03-14 17:28:57 +0200

    Remove lint- from package name

--- tests/test_bake.py
+++ tests/test_bake.py
@@ -2,5 +2,5 @@ def test_defaults(cookies):
     result = cookies.bake()
     assert result.exit_code == 0
     assert result.exception is None
-    assert result.project.basename == "flake8-lint-if-statements"
+    assert result.project.basename == "flake8-if-statements"
     assert result.project.isdir()
