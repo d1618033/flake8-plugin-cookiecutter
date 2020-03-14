698f71014b56387a04083b51310fa14251c6872d
d1618033 <d1618033@gmail.com>
2020-03-14 17:07:24 +0200

    Add basic test

---
+++ tests/test_bake.py
@@ -0,0 +1,6 @@
+def test_defaults(cookies):
+    result = cookies.bake()
+    assert result.exit_code == 0
+    assert result.exception is None
+    assert result.project.basename == "flake8-lint-if-statements"
+    assert result.project.isdir()
